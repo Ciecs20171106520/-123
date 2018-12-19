3bf0c3e85097c812730c0c35aceca6eaa8b9fa11
Ciecs20171106520 <694522707@qq.com>
2018-12-19 09:50:42 +0800

    2位加减乘除
    2个数字加减乘除

--- 计算器/ViewController.swift
+++ 计算器/ViewController.swift
@@ -11,55 +11,90 @@ import UIKit
 class ViewController: UIViewController {
     var temp:Double = 0
     var str:Double = 0
-    var temp1:Double = 0
-    var some:Double = 0
+    var some1:Double = 0
     var i:Double = 0
-    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var result: UITextField!
     @IBAction func button1(_ sender: Any) {
+        if i == 1
+        {
+         result.text=""
+        }
         result.text=result.text!+"1"
     }
     @IBAction func button2(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"2"
-        
     }
     @IBAction func button3(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"3"
     }
     @IBAction func button4(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
     result.text=result.text!+"4"
     }
     @IBAction func button5(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"5"
     }
     @IBAction func button6(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"6"
     }
     @IBAction func button7(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"7"
     }
     @IBAction func button9(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"9"
     }
     @IBAction func ac0(_ sender: Any) {
         temp=0
-        temp1=0
+        str = 0
         result.text=""
-        text1.text=""
     }
     @IBAction func button8(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"8"
     }
     @IBAction func button0(_ sender: Any) {
+        if i == 1
+        {
+            result.text=""
+        }
         result.text=result.text!+"0"
     }
     @IBAction func jisuan(_ sender: Any) {
-     if i==0
-        {
       if str==1
         {
             temp=temp+Double(result.text!)!
             result.text="\(temp)"
-            some=temp
+            //some1=temp
             //123mmm
         }
         if str==2
@@ -78,47 +113,49 @@ class ViewController: UIViewController {
             temp=temp/Double(result.text!)!
             result.text="\(temp)"
         }
-            i=1
-    }
+        i = 1
+    }/*
         if i==1
         {
-            result.text="\(temp)"
-           
-        }
+         result.text="\(temp)"
+         
+         }
          temp =  Double(result.text!)!
-    }
+ */
+    
         
     @IBAction func jia(_ sender: Any) {
-        
-        temp = temp + Double(result.text!)!
+        if temp == 0
+        {
+        temp = Double(result.text!)!
+        result.text=""
+        }
+        /*if str == 1
+        {
+        temp = Double(result.text!)!+temp
         //temp=Double(result.text!)!
         result.text=""
-        text1.text="\(temp)"
+        }*/
         str=1
-
     }
     @IBAction func jian(_ sender: Any) {
-        text1.text="\(temp)"
-        /*if(temp==0)
+        if temp==0
         {
             temp=Double(result.text!)!
             result.text=""
-        }*/
-        if str==2
-        {
-            
-        temp=temp-Double(result.text!)!
-        result.text=""
-        //temp=Double(result.text!)!
-        result.text="\(temp)"
-        result.text=""
-        str=2
         }
+        /*if str == 2
+        {
+        temp=temp-Double(result.text!)!
+        //temp=Double(result.text!)!
+        result.text=""
+        }*/
+        str=2
         }
     @IBAction func cheng(_ sender: Any) {
       
-        text1.text="\(temp)"
-        if(temp1==0)
+       /* text1.text="\(temp)"
+        iftemp ==0
         {
             temp=1
             temp1=1
@@ -130,9 +167,22 @@ class ViewController: UIViewController {
             result.text=""
         }
         str=3
+        }*/
+        if temp==0
+        {
+            temp=Double(result.text!)!
+            result.text=""
+        }
+        /*if str == 2
+         {
+         temp=temp-Double(result.text!)!
+         //temp=Double(result.text!)!
+         result.text=""
+         }*/
+        str=3
         }
     @IBAction func chu(_ sender: Any) {
-            str=4
+         /*
         text1.text="\(temp)"
         if(temp1==0)
         {
@@ -145,9 +195,14 @@ class ViewController: UIViewController {
             temp=temp/Double(result.text!)!
             //temp=Double(result.text!)!
             result.text=""
-}
-}
-    @IBOutlet weak var result: UITextField!
+}*/
+        if temp==0
+        {
+            temp=Double(result.text!)!
+            result.text=""
+        }
+        str=4
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
